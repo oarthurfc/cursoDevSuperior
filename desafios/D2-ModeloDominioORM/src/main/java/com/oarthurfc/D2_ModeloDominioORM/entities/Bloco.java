127c813d7efe07a05c4d7d37da2f482e7d844909
oarthurfc <arthuryouface@gmail.com>
2024-06-25 17:23:59 -0300

    Creating entities

---
+++ desafios/D2-ModeloDominioORM/src/main/java/com/oarthurfc/D2_ModeloDominioORM/entities/Bloco.java
@@ -0,0 +1,16 @@
+package com.oarthurfc.D2_ModeloDominioORM.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Instant inicio;
+    private  Instant fim;
+}
